c8131948defaef855a0b0f8a57195a499c8b2da9
Ian Godtliebsen <ian@chem.au.dk>
2017-02-01 15:08:22 +0100

    updated submit sctip

--- submit_slurm_grendel.sh
+++ submit_slurm_grendel.sh
@@ -3,7 +3,7 @@
 #SBATCH --partition=q28
 #SBATCH --job-name mytest     # name of job
 #SBATCH --ntasks-per-node 3   # SLURM_NTASKS_PER_NODE
-#SBATCH --nodes 1             # SLURM_JOB_NUM_NODES
+#SBATCH --nodes 2             # SLURM_JOB_NUM_NODES
 #SBATCH --time 1:00:00        # max time (HH:MM:SS)
 #
 
@@ -20,16 +20,20 @@ export OMP_NUM_THREADS=1
 export MKL_NUM_THREADS=1
 
 # Modules
-module purge
-module load openmpi/gnu-slurm/2.0.1
+#module purge
+#module load openmpi/gnu-slurm/2.0.1
+ml purge
+#ml openmpi/gnu-slurm/1.6.5
+#ml openmpi/intel-slurm/1.10.5
+ml openmpi/intel17-slurm/2.0.2
 
 mpirun --version
 
 # create hostfile
 echo "Creating host file"
-echo $SLURM_JOB_NODELIST > $SLURM_SUBMIT_DIR/hostfile
+echo $SLURM_JOB_NODELIST | $SLURM_SUBMIT_DIR/parse_slurm_nodelist.py > $SLURM_SUBMIT_DIR/hostfile
 sed -e 's/$/ slots=3 max_slots=20/' -i $SLURM_SUBMIT_DIR/hostfile
 
 #Run Midas
 echo "Running program"
-mpirun -n 1 --hostfile $SLURM_SUBMIT_DIR/hostfile $SLURM_SUBMIT_DIR/master $SLURM_SUBMIT_DIR/printer
+mpirun --map-by node -n 2 --hostfile $SLURM_SUBMIT_DIR/hostfile $SLURM_SUBMIT_DIR/master $SLURM_SUBMIT_DIR/printer
